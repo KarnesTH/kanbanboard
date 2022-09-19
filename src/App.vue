2ee8d2d66eee5bab8297284d67a8dc40d76af2fc
KarnesTH <p_haehnel@hotmail.de>
2022-09-19 20:07:49 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div
+        class="col-4"
+        v-for="statusCard in statusCards"
+        :key="statusCard.status"
+      >
+        <StatusCard
+          :title="statusCard.title"
+          :titleClasses="statusCard.titleClasses"
+          :newTasks="statusCard.newTasks"
+          :status="statusCard.status"
+          :tasks="filteredTasks(statusCard.status)"
+          @new-task="addTask"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StatusCard from "./components/StatusCard.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    StatusCard,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          content: "Dashboard überarbeiten.",
+          status: 0,
+        },
+        {
+          id: 2,
+          content: "Anwendung auf Vue.js umstellen.",
+          status: 0,
+        },
+      ],
+      statusCards: [
+        {
+          title: "Neue Aufgaben",
+          titleClasses: "bg-secondary text-white",
+          newTasks: true,
+          status: 0,
+        },
+        {
+          title: "In Bearbeitung",
+          titleClasses: "bg-primary text-white",
+          newTasks: false,
+          status: 1,
+        },
+        {
+          title: "Erledigt",
+          titleClasses: "bg-success text-white",
+          newTasks: false,
+          status: 2,
+        },
+      ],
+    };
+  },
+  methods: {
+    filteredTasks(status) {
+      return this.tasks.filter((task) => task.status === status);
+    },
+    addTask(task) {
+      task.id = this.tasks.length + 1;
+      this.tasks.push(task);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.min.css";
 </style>
---
+++ src/components/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="alert" :class="alertClass">{{ task.content }}</div>
+</template>
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+    alertColor: {
+      type: String,
+      default: "secondary",
+    },
+  },
+  computed: {
+    alertClass() {
+      return `alert-${this.alertColor}`;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
